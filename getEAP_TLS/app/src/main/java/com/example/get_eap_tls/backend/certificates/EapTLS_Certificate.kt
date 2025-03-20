b7358f9bf32ecffe8faf0cae77ab7491e0b08b26
Pablo Diz <p.dcruz@udc.es>
2025-03-20 16:31:38 +0100

    #2 DEV Isolated client private key from client certificate

--- getEAP_TLS/app/src/main/java/com/example/get_eap_tls/backend/certificates/EapTLS_Certificate.kt
+++ getEAP_TLS/app/src/main/java/com/example/get_eap_tls/backend/certificates/EapTLS_Certificate.kt
@@ -4,24 +4,39 @@ import java.security.PrivateKey
 import java.security.cert.X509Certificate
 import java.io.InputStream
 import java.security.cert.CertificateFactory
-import java.security.KeyStore
+import java.security.KeyFactory
+import java.security.spec.PKCS8EncodedKeySpec
+import java.util.Base64
 
 
-class EapTLSCertificate(caInputStream: InputStream,  clientInputStream: InputStream, val privateKeyPassword: String) {
+class EapTLSCertificate(caInputStream: InputStream,  
+                        clientCertInputStream: InputStream, 
+                        clientKeyInputStream: InputStream) {
     val caCertificate: X509Certificate
     val clientPrivateKey: PrivateKey
     val clientCertificate: X509Certificate
-    init{
-        // Obtengo el certificado x509  
+    init {
+        // Obtengo el certificado CA (X.509)
         val certificateFactory = CertificateFactory.getInstance("X.509")
-        caCertificate =
-            certificateFactory.generateCertificate(caInputStream) as X509Certificate
+        caCertificate = certificateFactory.generateCertificate(caInputStream) as X509Certificate
 
-        // Obtengo la clave y certificado x509 de usuario  
-        val keyStore = KeyStore.getInstance("PKCS12")
-        keyStore.load(clientInputStream, privateKeyPassword.toCharArray())
-        val alias = keyStore.aliases().nextElement()
-        clientCertificate = keyStore.getCertificate(alias) as X509Certificate
-        clientPrivateKey = keyStore.getKey(alias, "password".toCharArray()) as PrivateKey
+        // Obtengo el certificado del cliente (X.509)
+        clientCertificate = certificateFactory.generateCertificate(clientCertInputStream) as X509Certificate
+
+        // Obtengo la clave privada del cliente
+        val keyBytes = clientKeyInputStream.readBytes()
+        val keySpec = PKCS8EncodedKeySpec(decodePem(keyBytes))
+        val keyFactory = KeyFactory.getInstance("RSA")
+        clientPrivateKey = keyFactory.generatePrivate(keySpec)
+    }
+
+    // Función para decodificar un archivo PEM si tiene encabezados y pies de página
+    private fun decodePem(pem: ByteArray): ByteArray {
+        val pemString = String(pem)
+        val base64Content = pemString
+            .replace("-----BEGIN PRIVATE KEY-----", "")
+            .replace("-----END PRIVATE KEY-----", "")
+            .replace("\\s".toRegex(), "")
+        return Base64.getDecoder().decode(base64Content)
     }
 }
